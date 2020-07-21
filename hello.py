c07f9fd7df10edc02c3ecbe06bcd68dafe17660e
iamchahu <chathurangacithrasena@gmail.com>
2020-07-21 12:32:52 +0530

    python jupyter note book

--- hello.py
+++ hello.py
@@ -1,11 +1,1 @@
-from array import  *
-
-arr = array('i', [])
-n = int(input("Enter the length of the array: "))
-
-for i in range(5):
-    x = int(input("Enter the next value: "))
-    arr.append(x)
-
-print(arr)
-
+from numpy import *
